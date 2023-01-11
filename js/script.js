b08390a535f6c02f29f7bfa834d1a88311a5bddf
Franklin da Silva Batista <frsbatist@gmail.com>
2023-01-11 14:28:21 -0300

    ...

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const openModalButton = document.querySelector("#open-modal");
+const closeModalbutton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+
+const toggleModal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+};
+ 
+[openModalButton, closeModalbutton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
